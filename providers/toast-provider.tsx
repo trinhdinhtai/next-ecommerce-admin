c289cf1ec032c9c14a0da8c1f5b0d52a43838f06
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2023-08-26 00:42:51 +0700

    feat: update toast darkmode

--- providers/toast-provider.tsx
+++ providers/toast-provider.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { useTheme } from "next-themes";
 import { Toaster } from "react-hot-toast";
 
 const ToastProvider = () => {
-  return <Toaster position="top-right" />;
+  const { theme } = useTheme();
+  return (
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        style: {
+          background: theme === "dark" ? "#333" : "#fff",
+          color: theme === "dark" ? "#fff" : "#333",
+        },
+      }}
+    />
+  );
 };
 
 export default ToastProvider;
