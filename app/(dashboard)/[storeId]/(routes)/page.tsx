fcb268ee104b2b834eae9bbb0896f8819c5d41cf
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2023-08-26 00:31:50 +0700

    feat: add dashboard page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div></div>;
+import DashboardTabs from "@/components/DashboardTabs";
+import DateRangePicker from "@/components/ui/date-range-picker";
+
+export default function DashboardPage() {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <DateRangePicker />
+      </div>
+      <DashboardTabs />
+    </div>
+  );
 }
