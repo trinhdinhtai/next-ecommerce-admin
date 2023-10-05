919a8665a7b20e69a8237f2f7cd33b6c4c410ee5
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-10-05 22:29:45 +0700

    feat: add account page

--- app/(dashboard)/dashboard/account/page.tsx
+++ app/(dashboard)/dashboard/account/page.tsx
@@ -1,5 +1,26 @@
+import { Metadata } from "next"
+
+import { env } from "@/env.mjs"
+import { Shell } from "@/components/ui/shell"
+import { UserProfile } from "@/components/auth/user-profile"
+import PageHeading from "@/components/PageHeading"
+
+export const metadata: Metadata = {
+  metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
+  title: "Account",
+  description: "Manage your account settings",
+}
+
 const AccountPage = () => {
-  return <div>AccountPage</div>
+  return (
+    <Shell>
+      <PageHeading title="Account" description="Manage your account settings" />
+
+      <section>
+        <UserProfile />
+      </section>
+    </Shell>
+  )
 }
 
 export default AccountPage
