8baddd192a925fa10c12561190f62468ed1ef08c
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-10-07 14:31:18 +0700

    feat: update dashboard loading skeleton

--- app/(dashboard)/dashboard/stores/[storeId]/loading.tsx
+++ app/(dashboard)/dashboard/stores/[storeId]/loading.tsx
@@ -1,40 +1,33 @@
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { Shell } from "@/components/ui/shell"
 import { Skeleton } from "@/components/ui/skeleton"
 
 export default function StoreIdLoading() {
   return (
-    <div className="space-y-6">
-      <Card>
-        <CardHeader className="space-y-2">
-          <Skeleton className="h-6 w-1/4" />
-          <Skeleton className="h-4 w-2/4" />
-        </CardHeader>
-        <CardContent>
-          <Skeleton className="h-10 w-40" />
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader className="space-y-2">
-          <Skeleton className="h-6 w-1/4" />
-          <Skeleton className="h-4 w-2/4" />
-        </CardHeader>
-        <CardContent>
-          <div className="grid w-full max-w-xl gap-4">
-            <div className="space-y-2.5">
-              <Skeleton className="h-4 w-32" />
-              <Skeleton className="h-6" />
-            </div>
-            <div className="space-y-2.5">
-              <Skeleton className="h-4 w-32" />
-              <Skeleton className="h-20" />
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter className="xs:flex-row flex flex-col gap-2">
-          <Skeleton className="h-10 w-32" />
-          <Skeleton className="h-10 w-32" />
-        </CardFooter>
-      </Card>
-    </div>
+    <Shell>
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <Skeleton className="h-10 w-[300px]" />
+      </div>
+
+      <div className="space-y-4">
+        <Skeleton className="h-10 w-[360px]" />
+
+        <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-3">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Card key={index}>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <Skeleton className="h-5 w-20" />
+                <Skeleton className="h-5 w-5" />
+              </CardHeader>
+              <CardContent className="space-y-1">
+                <Skeleton className="h-7 w-20" />
+                <Skeleton className="h-4 w-40" />
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </Shell>
   )
 }
