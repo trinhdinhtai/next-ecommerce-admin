025e57863fbbad25c0dd8d5be70b85318b1c6b1f
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-10-08 10:26:16 +0700

    feat: change store setting loading

--- app/(dashboard)/dashboard/stores/[storeId]/settings/loading.tsx
+++ app/(dashboard)/dashboard/stores/[storeId]/settings/loading.tsx
@@ -3,11 +3,12 @@
 import { Separator } from "@radix-ui/react-dropdown-menu"
 
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Shell } from "@/components/ui/shell"
 import { Skeleton } from "@/components/ui/skeleton"
 
 const StoreSettingLoading = () => {
   return (
-    <div>
+    <Shell>
       <div className="mb-6">
         <Skeleton className="h-9 w-40" />
         <Skeleton className="mb-3 mt-1 h-5 w-60" />
@@ -36,7 +37,7 @@ const StoreSettingLoading = () => {
           <Skeleton className="h-10 w-32" />
         </CardFooter>
       </Card>
-    </div>
+    </Shell>
   )
 }
 
