eb948c4154f6d13355b7cdbcafede7daf393ac42
taid-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-11-21 15:19:35 +0700

    feat: add afterAuth function to handle redirection after authentication based on user and route conditions

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,25 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server"
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs"
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/", "/api/:path*"],
-});
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/dashboard/stores"
+
+      const storeSelection = new URL(path, req.url)
+      return NextResponse.redirect(storeSelection)
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url })
+    }
+  },
+})
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+}
