dd2bd7e910ade68bfb221efb751dce7ab3e0d29a
taid-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-11-21 15:56:27 +0700

    chore: add support for HTTPS protocol in remotePatterns for images in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
         hostname: "uploadthing.com",
       },
       {
+        protocol: "https",
         hostname: "utfs.io",
       },
     ],
